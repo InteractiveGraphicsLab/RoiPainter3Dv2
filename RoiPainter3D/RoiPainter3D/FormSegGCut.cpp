fe3a321b00003db004b1f9f54683b26bba8adf24
Takashi Ijiri <takashi.ijiri80@gmail.com>
2018-11-05 16:31:22 +0900

    forms for segGraphCut segRegionGrowing
    add forms for the two modes

---
+++ RoiPainter3D/RoiPainter3D/FormSegGCut.cpp
@@ -0,0 +1,17 @@
+#include "FormSegGCut.h"
+
+using namespace RoiPainter3D;
+
+
+
+
+System::Void FormSegGCut::btn_clearAllCPs_Click(System::Object^  sender, System::EventArgs^  e) {}
+System::Void FormSegGCut::trackBar_lambda_Scroll(System::Object^  sender, System::EventArgs^  e) {}
+System::Void FormSegGCut::btn_runGraphCutWS_Click(System::Object^  sender, System::EventArgs^  e) {}
+System::Void FormSegGCut::btn_runGraphCutVoxel_Click(System::Object^  sender, System::EventArgs^  e) {}
+System::Void FormSegGCut::rbtn_thick1_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {}
+System::Void FormSegGCut::rbtn_thick2_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {}
+System::Void FormSegGCut::rbtn_thick3_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {}
+System::Void FormSegGCut::checkBox_bandOnlyBack_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {}
+System::Void FormSegGCut::btn_canel_Click(System::Object^  sender, System::EventArgs^  e) {}
+System::Void FormSegGCut::btn_finish_Click(System::Object^  sender, System::EventArgs^  e) {}
