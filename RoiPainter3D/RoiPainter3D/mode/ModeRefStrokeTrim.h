0b5852c6ee7a587f315a2236c7471f0e998bec53
Takashi Ijiri <takashi.ijiri80@gmail.com>
2018-11-11 17:31:43 +0900

    implement Stroke trimming
    add ModeRefStrokeTrim

--- RoiPainter3D/RoiPainter3D/mode/ModeRefStrokeTrim.h
+++ RoiPainter3D/RoiPainter3D/mode/ModeRefStrokeTrim.h
@@ -17,6 +17,13 @@ class ModeRefStrokeTrim : public ModeInterface
   GlslShaderVolume m_volumeShader;
   GlslShaderCrsSec m_crssecShader;
 
+	bool m_bModified ;
+	bool m_bDrawStrok;
+	int  m_trgtMskId ;
+
+	byte  *m_volPrev;
+	vector<EVec2i> m_stroke2D;
+	vector<EVec3f> m_stroke3D;
 
   ModeRefStrokeTrim();
 public:
@@ -51,8 +58,10 @@ public:
   void cancelSegmentation();
   void finishSegmentation();
 
-
+private:
+  void updateVolFlgByStroke( OglForCLI *ogl);
 };
+
 
 #pragma managed
 
